e917018013f0b7036342ed0465d7f5b57c11621c
piraxis2 <kate107852@gmail.com>
2025-01-11 15:50:35 +0900

    적 찾기 데코레이터 작업중

--- Assets/Script/Article/CharacterArticle.cs
+++ Assets/Script/Article/CharacterArticle.cs
@@ -1,10 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using AutoCrawler.addons.behaviortree;
 using AutoCrawler.Assets.Script.Article.Interface;
 using AutoCrawler.Assets.Script.AutoCrawlerBehaviorTree.Action;
 using AutoCrawler.Assets.Script.TurnAction;
 using AutoCrawler.Assets.Script.TurnAction.Skill;
+using Godot;
 
 namespace AutoCrawler.Assets.Script.Article;
 
@@ -12,6 +14,7 @@ public partial class CharacterArticle : ArticleBase, ITurnAffected<ArticleBase>
 {
     private BehaviorTree _behaviorTree;
     private TurnActionBase _currentTurnAction;
+
     public TurnActionBase CurrentTurnAction
     {
         get => _currentTurnAction;
@@ -36,6 +39,39 @@ public partial class CharacterArticle : ArticleBase, ITurnAffected<ArticleBase>
         }
     }
 
+    public HashSet<Vector2I> AttackRangePositions
+    {
+        get
+        {
+            HashSet<Vector2I> GetAdjacentTiles(Vector2I position)
+            {
+                return new HashSet<Vector2I>
+                {
+                    position + new Vector2I(1, 0), // 동
+                    position + new Vector2I(-1, 0), // 서
+                    position + new Vector2I(0, 1), // 남
+                    position + new Vector2I(0, -1) // 북
+                };
+            }
+
+            HashSet<Vector2I> strikingArea = new(GetAdjacentTiles(TilePosition));
+            HashSet<Vector2I> completedArea = new() { TilePosition };
+
+            for (int i = 1; i < MinStrikingDistance; i++)
+            {
+                foreach (var area in strikingArea.ToList())
+                {
+                    if (completedArea.Add(area))
+                    {
+                        strikingArea.UnionWith(GetAdjacentTiles(area));
+                    }
+                }
+            }
+
+            return strikingArea;
+        }
+    }
+
     public Constants.BtStatus TurnPlay(double delta)
     {
         if (BehaviorTree == null) throw new NullReferenceException("BehaviorTree is null");
@@ -43,12 +79,12 @@ public partial class CharacterArticle : ArticleBase, ITurnAffected<ArticleBase>
         if (CurrentTurnAction != null)
         {
             TurnActionBase.ActionState actionState = CurrentTurnAction.Action(delta, this);
-            
+
             if (actionState == TurnActionBase.ActionState.End) _currentTurnAction = null;
-            
+
             return actionState == TurnActionBase.ActionState.Running ? Constants.BtStatus.Running : Constants.BtStatus.Success;
         }
-       
+
         return BehaviorTree.Behave(delta, this);
     }
 }
---
+++ Assets/Script/BattleFieldScene.cs
@@ -0,0 +1,16 @@
+﻿using Godot;
+
+namespace AutoCrawler.Assets.Script;
+
+public partial class BattleFieldScene : Node2D 
+{
+    public T GetBattleFieldCoreNode<T>() where T : Node
+    {
+        return typeof(T) switch
+        {
+            var t when t == typeof(BattleFieldTileMapLayer) => GetNode<T>("TileMapLayer"),
+            var t when t == typeof(TurnHelper) => GetNode<T>("TurnHelper"),
+            _ => null
+        };
+    }
+}
