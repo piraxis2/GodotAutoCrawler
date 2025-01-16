e870e592c696b5aebec3c52d6c1a82f15f84770d
piraxis <kate107852@gmail.com>
2025-01-16 18:42:38 +0900

    레이팅 셀렉터 수정

--- addons/behaviortree/node/Rating/BehaviorTree_RatingSelector.cs
+++ addons/behaviortree/node/Rating/BehaviorTree_RatingSelector.cs
@@ -7,10 +7,31 @@ namespace AutoCrawler.addons.behaviortree.node.Rating
 {
     public partial class BehaviorTree_RatingSelector : BehaviorTree_Composite
     {
+        private BehaviorTree_RatingDecorator _highestRatingDecorator;
+
         protected override Constants.BtStatus OnBehave(double delta, Node owner)
         {
+            if (_highestRatingDecorator == null)
+            {
+                _highestRatingDecorator = TakeDecorator(owner);
+                
+                if (_highestRatingDecorator == null) return Constants.BtStatus.Failure;
+            }
+
+            var status = _highestRatingDecorator.Behave(delta, owner);
+
+            if (status is Constants.BtStatus.Success or Constants.BtStatus.Failure)
+            {
+                _highestRatingDecorator = null;
+            }
+
+            return status;
+        }
+
+        BehaviorTree_RatingDecorator TakeDecorator(Node owner)
+        {
             // Stopwatch for //1
-           // Stopwatch stopwatch1 = Stopwatch.StartNew();
+            // Stopwatch stopwatch1 = Stopwatch.StartNew();
 
             int highestRating = 0;
             BehaviorTree_RatingDecorator highestRatingDecorator = null;
@@ -39,8 +60,7 @@ namespace AutoCrawler.addons.behaviortree.node.Rating
             //
             // stopwatch2.Stop();
             // GD.Print($"//2 Execution Time: {stopwatch2.ElapsedTicks} ticks");
-
-            return highestRatingDecorator?.Behave(delta, owner) ?? Constants.BtStatus.Failure;
+            return highestRatingDecorator;
         }
     }
 }
